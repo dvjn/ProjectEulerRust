c9df855c8f1fc19e442e0c005575aae6ee61f430
Divy Jain <dkj@somaiya.edu>
2021-07-18 17:24:05 +0530

    ✏️ fix typos

--- src/problem_0009.rs
+++ src/problem_0009.rs
@@ -1,10 +1,10 @@
 //! Special Pythagorean triplet
 
 pub fn solve() -> u64 {
-    product_of_pythgorean_triplet_with_sum(1000)
+    product_of_pythagorean_triplet_with_sum(1000)
 }
 
-pub fn product_of_pythgorean_triplet_with_sum(sum: u64) -> u64 {
+pub fn product_of_pythagorean_triplet_with_sum(sum: u64) -> u64 {
     let sum_squared = sum.pow(2);
     for a in 1..=(sum - 2) {
         for b in 1..=(sum - 1 - a) {
@@ -20,15 +20,15 @@ pub fn product_of_pythgorean_triplet_with_sum(sum: u64) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    use super::product_of_pythgorean_triplet_with_sum;
+    use super::product_of_pythagorean_triplet_with_sum;
 
     #[test]
     fn given_example() {
-        assert_eq!(product_of_pythgorean_triplet_with_sum(12), 60);
+        assert_eq!(product_of_pythagorean_triplet_with_sum(12), 60);
     }
 
     #[test]
     fn given_problem() {
-        assert_eq!(product_of_pythgorean_triplet_with_sum(1000), 31875000);
+        assert_eq!(product_of_pythagorean_triplet_with_sum(1000), 31875000);
     }
 }
